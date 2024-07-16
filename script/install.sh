4bc3d0ca3194bc34c73d311d60ed83dc23daa064
Jem <0x0xjem@gmail.com>
2024-07-16 13:40:09 +0400

    Use forge soldeer. Modify configuration to new format.

--- script/install.sh
+++ script/install.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 echo ""
-echo "*** Installing soldeer"
-cargo install soldeer
-echo "    Done"
-
-echo ""
 echo "*** Installing forge dependencies"
 forge install
 echo "    Done"
 
 echo ""
 echo "*** Installing soldeer dependencies"
-soldeer install
+rm -rf dependencies/* && forge soldeer update
 echo "    Done"
