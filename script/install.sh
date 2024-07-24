c626a179db1ae4ee809be5c7ee7457af17d27779
Jem <0x0xjem@gmail.com>
2024-07-24 17:20:58 +0400

    Apply patch to Baseline imports to be compatible with soldeer remappings. Add missing dependencies.

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 echo ""
+echo "*** Removing submodules"
+rm -rf lib/
+
+echo ""
 echo "*** Setting up submodules"
 git submodule init
 git submodule update
@@ -16,6 +20,10 @@ echo "*** Restoring submodule commits"
 echo ""
 echo "baseline"
 cd lib/baseline-v2/ && git checkout 60bed78b7bee28016321ddd8c590df6c61bae6e9  && cd ../..
+
+echo ""
+echo "*** Applying patch to Baseline submodule"
+patch -d lib/baseline-v2/ -p1 < script/baseline.patch
 
 echo ""
 echo "*** Installing soldeer dependencies"
