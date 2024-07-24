89421060f3f29dd404c9bdf68c6174a9c161ffdd
Jem <0x0xjem@gmail.com>
2024-07-24 15:02:43 +0400

    Add git submodule for Baseline

--- script/install.sh
+++ script/install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
 echo ""
+echo "*** Setting up submodules"
+git submodule init
+git submodule update
+
+echo ""
 echo "*** Installing forge dependencies"
 forge install
 echo "    Done"
+
+echo ""
+echo "*** Restoring submodule commits"
+
+echo ""
+echo "baseline"
+cd lib/baseline-v2/ && git checkout 60bed78b7bee28016321ddd8c590df6c61bae6e9  && cd ../..
 
 echo ""
 echo "*** Installing soldeer dependencies"
