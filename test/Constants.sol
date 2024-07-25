e61e8e755ce268b4811bdf272434656cb3beecc4
Jem <0x0xjem@gmail.com>
2024-07-25 09:30:28 +0400

    Update salts (not sure why this was needed again)

--- test/Constants.sol
+++ test/Constants.sol
@@ -7,13 +7,13 @@ abstract contract TestConstants is TestConstantsCore {
     address internal constant _UNISWAP_V2_FACTORY =
         address(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
     address internal constant _UNISWAP_V2_ROUTER =
-        address(0xAAcF36e66E48D0fb8f12381daC69Aa9FA1C69159);
+        address(0xAAf663c9E2FE1EBE2a5930026d1ffEC4475b9608);
     address internal constant _UNISWAP_V3_FACTORY =
-        address(0xAAe4d0F0f763EC4777ae9419C6D83A566e884b77);
-    address internal constant _GUNI_FACTORY = address(0xAA62AaF5Bf60f9b7E9bc0C257785b38d039b805e);
+        address(0xAAd7024D1Fa43a88d975e441E18Dd26E5973e53A);
+    address internal constant _GUNI_FACTORY = address(0xAA7Fd572432bC7C7Ee41E81d24A98FfEee858a35);
     address internal constant _BASELINE_KERNEL = address(0xBB);
     address internal constant _BASELINE_QUOTE_TOKEN =
-        address(0xAA8fFfB79588c028237300E0B7F6F1923cca651C);
+        address(0xAA57CB5d789F3E3343eE786d94495507A94b7Fc7);
     address internal constant _CREATE2_DEPLOYER =
         address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
 }
