b3ad13d7ec17188a896fd875cbf3740042cfd3bd
Jem <0x0xjem@gmail.com>
2024-07-29 12:17:17 +0400

    Fix ordering of install script

--- script/install.sh
+++ script/install.sh
@@ -10,16 +10,16 @@ git submodule init
 git submodule update
 
 echo ""
-echo "*** Installing forge dependencies"
-forge install
-echo "    Done"
-
-echo ""
 echo "*** Restoring submodule commits"
 
 echo ""
 echo "baseline"
 cd lib/baseline-v2/ && git checkout 60bed78b7bee28016321ddd8c590df6c61bae6e9  && cd ../..
+
+echo ""
+echo "*** Installing forge dependencies"
+forge install
+echo "    Done"
 
 echo ""
 echo "*** Applying patch to Baseline submodule"
