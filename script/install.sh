23e375e95432e8849e1ea3fc11662617320dbae5
Jem <0x0xjem@gmail.com>
2024-08-19 12:12:39 +0400

    Cross-port installation script and formatting changes from #11

--- script/install.sh
+++ script/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 echo ""
+echo "*** Removing submodules"
+rm -rf lib/
+
+echo ""
+echo "*** Setting up submodules"
+git submodule init
+git submodule update
+
+echo ""
 echo "*** Installing forge dependencies"
 forge install
 echo "    Done"
