50012d25c4d466613093562ef72ad66062d2dadc
Jem <0x0xjem@gmail.com>
2025-03-04 16:07:00 +0400

    Disable Baseline constants

--- test/Constants.sol
+++ test/Constants.sol
@@ -11,9 +11,9 @@ abstract contract TestConstants is TestConstantsCore {
     address internal constant _UNISWAP_V3_FACTORY =
         address(0xAA3023F92819f02180b25795a5797f69F3627Cb7);
     address internal constant _GUNI_FACTORY = address(0xAA19F2E4084fd3e49C198e616e181B82332000D5);
-    address internal constant _BASELINE_KERNEL = address(0xBB);
-    address internal constant _BASELINE_QUOTE_TOKEN =
-        address(0xAABA4a4ef5c3C62a3F40e61BC675331662dB4D96);
+    // address internal constant _BASELINE_KERNEL = address(0xBB);
+    // address internal constant _BASELINE_QUOTE_TOKEN =
+    //     address(0xAABA4a4ef5c3C62a3F40e61BC675331662dB4D96);
     address internal constant _CREATE2_DEPLOYER =
         address(0x4e59b44847b379578588920cA78FbF26c0B4956C);
 
